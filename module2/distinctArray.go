f78d8fc114be7947ea277d5688933531bf67c305
manjusharami <manjusha84r2020@gmail.com>
2026-06-25 15:46:10 +0100

    second solution update

--- module2/distinctArray.go
+++ module2/distinctArray.go
@@ -17,3 +17,25 @@ func distinctArray(){
 	}
 	fmt.Println(result)
 }
+//1. Find the number of distinct elements in an array.//
+
+// package module2
+
+// import "fmt"
+
+// func distinctArray() {
+// 	array1 := []int{1, 3, 3, 3, 5, 2, 6, 8, 9, 8}
+// 	seen := make(map[int]bool)
+// 	unique := []int{}
+
+// 	for _, v := range array1 {
+		 
+// 		if !seen[v] {
+// 			unique = append(unique, v)
+// 			seen[v] = true
+// 		}
+
+// 	}
+// 	fmt.Println(unique)
+// 	fmt.Println(len(unique))
+// }
--- module2/distinctEle.go
+++ module2/distinctEle.go
@@ -21,25 +21,4 @@ func distincEle(){
 	fmt.Println("the array is",result)
 }
 
-//1. Find the number of distinct elements in an array.//
-
-// package module2
-
-// import "fmt"
-
-// func distinctArray() {
-// 	array1 := []int{1, 3, 3, 3, 5, 2, 6, 8, 9, 8}
-// 	seen := make(map[int]bool)
-// 	unique := []int{}
-
-// 	for _, v := range array1 {
-		 
-// 		if !seen[v] {
-// 			unique = append(unique, v)
-// 			seen[v] = true
-// 		}
-
-// 	}
-// 	fmt.Println(unique)
-// 	fmt.Println(len(unique))
-// }
+ 
