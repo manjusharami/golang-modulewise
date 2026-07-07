91f3a40ba08105e8ea4fbffe4c37e0d859eb3d26
manjusharami <manjusha84r2020@gmail.com>
2026-07-07 15:11:13 +0100

    error handling

--- main.go
+++ main.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"fmt"
-	"manju/module3"
+	"manju/module4"
 )
 
 func main() {
 	fmt.Println("Welcome to go")
 	// module1.Start()
 	// module2.Start()
-	module3.Start()
+	// module3.Start()
+	module4.Start()
 }
---
+++ module4/start.go
@@ -0,0 +1,28 @@
+package module4
+
+import "fmt"
+
+type MyError struct {
+	Err string
+}
+
+func (e MyError) LogError() {
+	fmt.Println(e.Err)
+}
+
+func Start() {
+	fmt.Println("I am in  module 4")
+
+	a, b := 10, 2000
+	var err MyError
+
+	if b == 0 {
+		err = MyError{Err: "Divide by zero is not allowed"}
+	} else if b > 1000 {
+		err = MyError{Err: "Larger b not allowed"}
+	} else {
+		fmt.Println(a / b)
+	}
+
+	err.LogError()
+}
