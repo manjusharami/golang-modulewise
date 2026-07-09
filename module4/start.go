dc3b3e1648a260118695c5c4fc487c43ee3f658e
manjusharami <manjusha84r2020@gmail.com>
2026-07-09 17:58:46 +0100

    recover

--- module4/start.go
+++ module4/start.go
@@ -1,22 +1,25 @@
 package module4
 
 import (
+	"errors"
 	"fmt"
-	"time"
+	"math"
 )
 
 type MyError struct {
 	Err string
 }
- // custom error handlers 
-func (e MyError) LogError() {
-	fmt.Println("Error Happened at" + time.Now().String() + "with the error" + e.Err)
+
+// custom error handler, returning error
+func (e MyError) LogError() error {
+	//fmt.Println("Error Happened at" + time.Now().String() + "with the error" + e.Err)
+	return fmt.Errorf(e.Err)
 }
 
 func Start() {
 	fmt.Println("I am in  module 4")
 
-	a, b := 10, 2000
+	a, b := 10, 0
 	var err MyError
 
 	if b == 0 {
@@ -27,5 +30,32 @@ func Start() {
 		fmt.Println(a / b)
 	}
 
-	err.LogError()
+	fmt.Println(err.LogError())
+
+	//sentinelError()
+
+	panicMethod()
+}
+
+// sentinel error are  reusable predefined errrors that colors can compare
+func sentinelError() {
+	var errFound = errors.New("Record not found")
+	a := []int{2, 3, 4, 5}
+	fmt.Println("sentinelError")
+	if len(a) > 6 {
+		fmt.Println(errFound)
+	}
+}
+
+func panicMethod() {
+	a := 1000.0
+	if a < 0 {
+		defer func() {
+			if r := recover(); r != nil {
+				fmt.Println("recovering from panic")
+			}
+		}()
+		panic("Negative nummber is not allowed")
+	}
+	fmt.Println(math.Sqrt(a))
 }
