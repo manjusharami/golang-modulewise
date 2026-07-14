bc6b53a48c7e23123d20c58925ab454c428d5dae
manjusharami <manjusha84r2020@gmail.com>
2026-07-14 18:47:29 +0100

    annonymous go routine

--- module5/start.go
+++ module5/start.go
@@ -9,13 +9,21 @@ func exampleOne() {
 	fmt.Println("Simple hello from examle one")
 }
 
-// func exampleTwo() {
-// 	fmt.Println("Simple hello from example two")
-// }
+func exampleTwo() {
+	fmt.Println("Simple hello from example two")
+
+	var i = 10
+	go func() {
+		fmt.Println(i)
+	}()
+}
 
 func Start() {
 	go fmt.Println("welcome to module5")
 
 	go exampleOne()
+
+	exampleTwo()
+	
 	time.Sleep(1 * time.Second)
 }
