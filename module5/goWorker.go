b3d12ac1bf2fd67257291eb61d26c1800423a6d5
manjusharami <manjusha84r2020@gmail.com>
2026-08-02 15:47:05 +0100

    add html

--- module5/goWorker.go
+++ module5/goWorker.go
@@ -2,20 +2,18 @@ package module5
 
 import (
 	"fmt"
-	"strconv"
-	"time"
+	"sync"
 )
 
-func Worker(id int) {
-
-	fmt.Println("worker" + strconv.Itoa(id) + "started")
-	time.Sleep(500 * time.Millisecond)
-	fmt.Println("worker" + strconv.Itoa(id) + "started")
-}
-
 func GoWorkerNode() {
-	go Worker(1)
-	go Worker(2)
-	time.Sleep(2 * time.Second)
+	var wg sync.WaitGroup
+	for i := 1; i <= 3; i++ {
+		wg.Add(1)
+		go func(n int) {
+			defer wg.Done()
+			fmt.Println(n)
+		}(i)
+	}
+	wg.Wait()
 
 }
